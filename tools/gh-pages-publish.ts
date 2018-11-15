7ff5592cc5f369ab57c350d47d2409d4e0525413
Hernan Rajchert <hrajchert@gmail.com>
2018-11-15 18:46:41 -0300

    fix(gh-pages-publish): Fixed docs publishing

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -2,20 +2,9 @@ const { cd, exec, echo, touch } = require('shelljs');
 const { readFileSync } = require('fs');
 const url = require('url');
 
-let repoUrl;
-let pkg = JSON.parse(readFileSync('package.json') as any);
-if (typeof pkg.repository === 'object') {
-  if (!pkg.repository.hasOwnProperty('url')) {
-    throw new Error('URL does not exist in repository section');
-  }
-  repoUrl = pkg.repository.url;
-} else {
-  repoUrl = pkg.repository;
-}
 
-let parsedUrl = url.parse(repoUrl);
-let repository = (parsedUrl.host || '') + (parsedUrl.path || '');
-let ghToken = process.env.GH_TOKEN || 'hrajchert';
+let repository = 'github.com/ts-task/fs';
+let ghToken = process.env.GH_TOKEN;
 
 echo('Deploying docs!!!');
 cd('docs');
@@ -24,9 +13,8 @@ exec('git init');
 exec('git add .');
 exec('git config user.name "Hernan Rajchert"');
 exec('git config user.email "hrajchert@gmail.com"');
-exec('git remote add origin git@github.com:ts-task/fs.git');
 exec('git commit -m "docs(docs): update gh-pages"');
 exec(
-  `git push --force --quiet origin master:gh-pages`
+  `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
 );
 echo('Docs deployed!!');
